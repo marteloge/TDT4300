03d11fe158eedc769514b0f101340d947269e17d
vikre <vikre.90@gmail.com>
2014-02-25 15:12:10 +0100

    Tror bruteforce er ok nå

--- AprioriAlgorithm/apriori/BruteForceApriori.java
+++ AprioriAlgorithm/apriori/BruteForceApriori.java
@@ -19,19 +19,48 @@ public class BruteForceApriori<V> extends BaseApriori<V> {
         * */
         List<ItemSet<V>> singles = getAllItemsetsOfSizeOne();
 
-        for (int i = 0; i < singles.size(); i++){
-            /*
-            * pruning
-            * */
-            List<ItemSet<V>> pruned = pruneInfrequentCandidates(minSupport,singles);
+        /*
+         * pruning
+         * */
+        List<ItemSet<V>> pruned = pruneInfrequentCandidates(minSupport,singles);
+
+        System.out.println("Level: "+1+"\n"+"\tGenerated "+singles.size() + " candidates.\n");
+        System.out.print("\t"+singles+"\n");
+        System.out.print(
+                "\tAnd kept " + pruned.size() + " frequent sets\n" +
+                        "\t" + pruned);
+        frequent1Itemsets = pruned;
+        frequentItemSets.put(1,pruned);
+
+        //Candidates for lower levels
+        List<ItemSet<V>> candidates = singles;
+
+        for (int i = 2; ; i++){
+
+            //Generate possible candidates
+            List<ItemSet<V>> generatedCandidates = aprioriGen(candidates);
+
+            System.out.println("Level: "+i+"\n"+
+                    "\tGenerated "+generatedCandidates.size() + " candidates.\n");
+            System.out.print("\t"+generatedCandidates+"\n");
+
+            //legge til settet i forrige sett
+            candidates = generatedCandidates;
+
+            //pruning
+            pruned = pruneInfrequentCandidates(minSupport,generatedCandidates);
+
+            System.out.print(
+                            "\tAnd kept " + pruned.size() + " frequent sets\n" +
+                            "\t" + pruned);
+
+            //hvis det ikke er mer å prune så er vi ferdige
+            if(pruned.size() == 0){
+                break;
+            }
 
             //Legge til i sett
             frequentItemSets.put(i,pruned);
-
-            //System.out.println("yiha");
-
-            //Generere kombinasjoner
-            singles = aprioriGen(singles);
         }
     }
 }
